f8ba5f42ecbdd604a729e0fccce99ebdccc2a493
Bryce <swarvingprogrammer@gmail.com>
2021-11-14 19:28:50 -0500

    Creates connect function.

--- src/shoggoth/build_node.py
+++ src/shoggoth/build_node.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 # Standard Python Libraries
-
+from ipaddress import ip_address
+import re
+import sys
 
 # Third-Party Libraries
 from pyVim.connect import SmartConnect
@@ -11,7 +13,34 @@ import stdiomask
 EMAIL_REGEX = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
 
 
-def connection():
+def connect():
+    vCenter_ip = get_vCenter_ip()
+    vCenter_user = get_vCenter_user()
+    vCenter_password = get_password()
+
+    while True:
+        try:
+            connection = SmartConnect(
+                host=vCenter_ip,
+                user=vCenter_user,
+                pwd=vCenter_password,
+                disableSslCertValidation=True,
+            )
+            break
+        except ConnectionRefusedError as err:
+            print(str(err))
+            print("Error with vCenter IP. Please check.")
+            vCenter_ip = get_vCenter_ip()
+
+        except pyVmomi.vim.fault.InvalidLogin as err:
+            print(err.msg)
+            print("Please re-enter the username and password.")
+            vCenter_user = get_vCenter_user()
+            vCenter_password = get_password()
+
+    return connection
+
+
 def get_vCenter_user():
     """Get the username for vCenter."""
     while True:
@@ -44,31 +73,56 @@ def get_vCenter_ip() -> str:
 
 
 def get_os_password():
+    pass
+
 
 def get_vmHost():
+    pass
+
 
 def check_default_NIC():
+    pass
+
 
 def get_datastore():
+    pass
+
 
 def get_operator_network():
+    pass
+
 
 def get_number_operator():
+    pass
+
 
 def build_networks():
+    pass
+
 
 def set_autostart():
+    pass
+
 
 def clone_vm():
+    pass
+
 
 def deploy_servers():
+    pass
+
 
 def deploy_kalis():
+    pass
+
 
 def deploy_commando():
+    pass
+
 
 def main():
     """Shoggoth Main"""
+    pass
 
 
 if __name__ == "__main__":
